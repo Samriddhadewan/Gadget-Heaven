65966f3c691d9103a80fc59b44c6a30d448da271
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-01-26 14:42:36 +0600

    no proiduct in this category logic added

--- src/Components/GadgetCard/GadgetCard.jsx
+++ src/Components/GadgetCard/GadgetCard.jsx
@@ -2,13 +2,13 @@
 const GadgetCard = ({gadget}) => {
     const {product_title, product_id,product_image,price} = gadget
   return (
-    <div className="w-[350px] p-3 rounded-lg bg-white">
+    <div className="w-[350px] p-5 rounded-lg bg-white">
         <div className="h-[250px] mb-4">
         <img className="w-full object-cover h-full rounded-lg " src={product_image} alt="" />
         </div>
         <h1 className="text-[#09080F] font-semibold text-2xl">{product_title}</h1>
-        <p>product price: {price}</p>
-        <button>View Details</button>
+        <p className="text-[#09080F99] my-3">product price: {price}</p>
+        <button className="text-[#9538E2] font-semibold p-4 rounded-full border border-[#9538E2]">View Details</button>
     </div>
   )
 }
--- src/Components/GadgetCards/GadgetCards.jsx
+++ src/Components/GadgetCards/GadgetCards.jsx
@@ -1,32 +1,39 @@
 import { useEffect, useState } from "react";
-import { useLoaderData, useParams } from "react-router-dom"
+import { useLoaderData, useParams } from "react-router-dom";
 import GadgetCard from "../GadgetCard/GadgetCard";
 
 const GadgetCards = () => {
-    const params = useParams();
-    const {category} = params;
-    const data = useLoaderData();
-    const [cards, setCards] = useState([])
+  const params = useParams();
+  const { category } = params;
+  const data = useLoaderData();
+  const [cards, setCards] = useState([]);
 
+  useEffect(() => {
+    if (category) {
+      const filterdDatas = [...data].filter(
+        (gadget) => gadget.category === category
+      );
+      setCards(filterdDatas);
+    } else {
+      setCards(data);
+    }
+  }, [category, data]);
 
-    console.log(category, data);
-    useEffect(()=>{
-        if(category){
-            const filterdDatas = [...data].filter(gadget => gadget.category === category);
-            setCards(filterdDatas)
-        }
-        else{
-            setCards(data);
-        }
-    
-    },[category, data])
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {
-            cards.map((gadget, idx) => <GadgetCard gadget={gadget} key={idx}></GadgetCard>)
-        }
+    <div>
+      {cards.length === 0 ? (
+        <p className="text-center text-gray-500 mt-6">
+          No products available in this category. Please select a different category.
+        </p>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {cards.map((gadget, idx) => (
+            <GadgetCard gadget={gadget} key={idx}></GadgetCard>
+          ))}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default GadgetCards
+export default GadgetCards;
