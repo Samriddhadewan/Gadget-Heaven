3f88ab366ffb79a65353aca18cf3f977f5945695
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-01-27 16:07:58 +0600

    check

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,16 +1,43 @@
-import { useLoaderData } from "react-router-dom"
+
 import { Helmet } from "react-helmet";
+import { useLoaderData } from "react-router-dom";
+import { Bar, BarChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
 
 const Statistics = () => {
-  const data = useLoaderData();
+  const data = useLoaderData()
   console.log(data)
+
+  const chartData = data.map((product) => ({
+    name: product.product_title,
+    price: product.price
+  }))
+
+
   return (
-    <div>
-      <Helmet>
-        <title>Statistics</title>
-      </Helmet>
-    </div>
+      <div className="my-5">
+        <h1 className="text-4xl font-semibold">Statistics</h1>
+            <div className=" h-[80vh] flex items-center">
+      
+      <div className="w-[80%] mx-auto  h-[400px]">
+  <Helmet>
+    <title>Statistics</title>
+  </Helmet>
+  <ResponsiveContainer width="100%" height="100%">
+    <BarChart data={chartData}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Bar dataKey="price" fill="#9538E2" />
+    </BarChart>
+  </ResponsiveContainer>
+
+  
+</div>
+</div>
+      </div>
+
   )
 }
 
